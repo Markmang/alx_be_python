521c2f2715a0d79ff1edad2414b8508ce56685dc
Markmang <markudeagha@gmail.com>
2025-08-03 10:17:41 +0100

    bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+
+class BankAccount :
+    def __init__(self, account_balance) :
+        self.account_balance = account_balance
+
+    def deposit(self,amount) :
+        self.amount = amount
+        self.account_balance = self.account_balance + amount
+    def withdraw(self,amount) :
+        self.amount = amount
+        if amount > self.account_balance :
+            return False
+        else :
+            self.account_balance = self.account_balance - self.amount
+            return True
+    def display_balance(self) :
+        print(f"Current Balance: ${self.account_balance}")
+
