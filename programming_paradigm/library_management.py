420c3311db95ce819fbe5847bd67fc7f04c96734
Markmang <markudeagha@gmail.com>
2025-08-03 15:44:15 +0100

    library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -3,6 +3,17 @@ class Book:
         self.title = title
         self.author = author
         self.is_checked_out = False
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+    
 class Library:
     def __init__(self):
         self._books = []
